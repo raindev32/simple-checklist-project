a158878346041031d52d9ef15cb916d89a25535c
jhonyrain32 <jhonylau95@gmail.com>
2020-11-29 20:51:54 +0700

    add vscode set and consume API login, register,etc

--- src/Content/Container.js
+++ src/Content/Container.js
@@ -1,32 +1,41 @@
 import React from 'react'
 
 class Container extends React.Component {
-    render() {
+    render () {
         const { data } = this.props
 
-        return(
+        return (
             <div>
-            <form method="POST" action="">
-                {
-                    data.map((item, index) => (
-                        <div>
-                        <input 
-                            type="checkbox" 
-                            name="checkbox1" 
-                        />
-                        <label>{item.name}</label>
-                    </div>
-                    ))
-                }
-                
-                <br />
-                <br />
-                <br />
-                <br />
-            </form>
-    </div>
+                <form method="POST" action="">
+                    {
+                        data.map((item, index) => (
+                            <div>
+                                <text key={index}>{item.name}</text>
+                                {
+                                    item
+                                    && item.items
+                                    && item.items.map((opsi, index) => (
+                                        <div key={index}>
+                                            <input
+                                                type="checkbox"
+                                                name="checkbox1"
+                                                checked={opsi.itemCompletionStatus}
+                                            />
+                                            <label>{opsi.name}</label>
+                                        </div>
+                                    ))
+                                }
+                                <br />
+                            </div>
+                        ))
+                    }
+
+                    <br />
+                    <br />
+                    <br />
+                </form>
+            </div>
         )
     }
 }
-export default  Container
-  
+export default Container
